05630c48aeb9ed28d379659340bc2f89b68c9376
Pasquale De Marinis <pasquale199876@hotmail.it>
2025-04-12 18:02:41 +0200

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,14 +35,14 @@ for (let i = 0; i < navbarLinks.length; i++) {
  * back to top & header
  */
 
-const backTopBtn = document.querySelector("[data-back-to-top]");
+// const backTopBtn = document.querySelector("[data-back-to-top]");
 
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-});
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY >= 100) {
+//     header.classList.add("active");
+//     backTopBtn.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//     backTopBtn.classList.remove("active");
+//   }
+// });
